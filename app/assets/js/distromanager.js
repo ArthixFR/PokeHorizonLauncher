e58907dca23c0364ae821ea11026235c3bff9235
Julien <ArthixFR@users.noreply.github.com>
2025-03-26 22:15:17 +0100

    Ajout de l'URL en brut

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = process.env.REMOTE_DISTRO_URL
+exports.REMOTE_DISTRO_URL = 'https://cdn.arthix.dev/pokehorizon_launcher/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
